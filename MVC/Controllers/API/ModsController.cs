aaa2a18d93a76bb15991a74216b13170483656f9
FedericoLaptop <federicoarredondoc@gmail.com>
2022-06-25 13:45:25 -0300

    feat: added Mods route to API

--- MVC/Controllers/API/ModsController.cs
+++ MVC/Controllers/API/ModsController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MVC.Models.DTO;
+using Objects;
+using Objects.Repositories.Interfaces;
+using System.Collections.Generic;
 
 namespace MVC.Controllers.API
 {
@@ -7,11 +11,48 @@ namespace MVC.Controllers.API
     [ApiController]
     public class ModsController : ControllerBase
     {
-        // GET api/<ComprasController>/5
-        [HttpGet, Route("GetName")]
-        public ActionResult<string> GetName()
+        private IModRepository modsRepo;
+
+        public ModsController(IModRepository modsRepo)
         {
-            return Ok("name");
+            this.modsRepo = modsRepo;
+        }
+
+        // GET api/<ComprasController>/5
+        [HttpGet, Route("Mods")]
+        public ActionResult<List<ModToSendDTO>> Mods([FromBody] ModsDTO modsList)
+        {
+            if (modsList == null || modsList.mods == null)
+                return BadRequest();
+
+            List<ModToSendDTO> modsToUpdate = new List<ModToSendDTO>();
+            try
+            {
+                foreach(var m in modsList.mods)
+                {
+                    Mod mod = modsRepo.FindById(m.modId);
+                    if(mod != null)
+                    {
+                        if(mod.Version != m.version)
+                        {
+                            ModToSendDTO mts = new ModToSendDTO();
+                            mts.mod_id = mod.ModId;
+                            mts.file_name = mod.FileName;
+                            mts.current_download_link = mod.DownloadLink;
+                            mts.current_version = mod.Version;
+                            mts.mod_name = mod.Name;
+
+                            modsToUpdate.Add(mts);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return Problem();
+            }
+
+            return Ok(modsToUpdate);
         }
     }
 }
---
+++ MVC/Models/DTO/ModToSendDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MVC.Models.DTO
+{
+    public class ModToSendDTO
+    {
+        public string mod_id { get; set; }
+        public string mod_name { get; set; }
+        public string current_version { get; set; }
+        public string current_download_link { get; set; }
+        public string created_by { get; set; }
+        public string file_name { get; set; } 
+        public DateTime last_update { get; set; }
+    }
+}
